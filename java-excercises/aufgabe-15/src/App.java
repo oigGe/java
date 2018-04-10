15a6d9effdae21abcde87089d999c6b6add549c8
georg.g <gross-georg@web.de>
2018-04-10 21:37:46 +0200

    aufgabe15 added

---
+++ java-excercises/aufgabe-15/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 5;
+		System.out.println(a +"    "+ b);
+
+		int c = a;
+		a = b;
+		b = c;
+		System.out.println(a + "    " + b);
+
+	}
+}
